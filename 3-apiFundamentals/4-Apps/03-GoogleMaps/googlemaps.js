fd259d03ebd318f5c099fe2f5fb57c63da396989
Brad Dozier <63806708+braddozier93@users.noreply.github.com>
2020-08-06 15:28:32 -0400

    notes added for googlemaps api

--- 3-apiFundamentals/4-Apps/03-GoogleMaps/googlemaps.js
+++ 3-apiFundamentals/4-Apps/03-GoogleMaps/googlemaps.js
@@ -1,7 +1,15 @@
+//check to see if the browser can find the geolocation information. if there is, we'll use getCurrentPosition() on that geolocation...
+//if there is no geolocation, we obviously can't use it in our map, so we'll need to create a p tag on the html page and fill it with an error string,,,
+//this way the user can see if there is an error
+//
+
+
+
 if("geolocation" in navigator) {
     navigator.geolocation.getCurrentPosition(function(position) {
-
+//take the geolocation given to use from our browser as position. then we use that position info to get the lat and long to use in our LatLng constructor.
     var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    //adding options from the googlemaps api docs. these can be played around with. link to these options is in module "options for map"
     var myOptions = {
         zoom: 8,
         center: latlng,
@@ -9,8 +17,9 @@ if("geolocation" in navigator) {
         disableDefaultUI: true
 
     }
+    //use our Map constructor to create a map on the HTML element with an id of "map_canvas" using the options we've set up
     var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
-    // document.body.appendChild(map);
+    // document.body.appendChild(map); --------don't need this
 
     });
     } else {
